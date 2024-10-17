9ca0ae37287ac06d5fb143c808de4a6aca5e4f58
Stiven Rivera <115115438+stivenrivera@users.noreply.github.com>
2024-10-17 15:55:20 -0500

    Update python.py

--- python.py
+++ python.py
@@ -1,76 +1,139 @@
 class itemz:
 
     def __init__(self, name, price, qty):
+
         self.name = name
+
         self.price = price
+
         self.qty = qty
+
         self.category = "general"
+
         self.env_fee = 0
 
-    def get_total(self):
+
+
+    def getTotal(self):
+
         return self.price * self.qty
 
-    def get_most_prices(self):
+    def getTMostPrices(self):
+
         return self.price * self.qty * 0.6
 
-# class to shop
 
 
-class shoppin_cart:
+#class to shop
+
+class shoppinCart:
 
     def __init__(self):
+
         self.items = []
-        self.tax_rate = 0.08
-        self.member_discount = 0.05
-        self.big_spender_discount = 10
-        self.coupon_discount = 0.15
+
+        self.taxRate = 0.08
+
+        self.memberDiscount = 0.05
+
+        self.bigSpenderDiscount = 10
+
+        self.couponDiscount = 0.15
+
         self.currency = "USD"
 
-    def add_item(self, item):
-        # discount added here
+
+
+    def addItem(self, item):
+
+#discount added here
+
         self.items.append(item)
 
-    def calculate_subtotal(self):
-        # todo: fix this in the future i guess
+
+
+    def calculateSubtotal(self):
+
+#todo: fix this in the future i guess
+
         subtotal = 0
+
         for item in self.items:
+
             subtotal += item.getTotal()
+
         return subtotal
 
-    def apply_discounts(self, subtotal, is_member):
-        if is_member == "yes":
-            subtotal = subtotal - (subtotal * self.member_discount)
+
+
+    def applyDiscounts(self, subtotal, isMember, hasCoupon):
+
+        if isMember == "yes":
+
+            subtotal = subtotal - (subtotal * self.memberDiscount)
+
         if subtotal > 100:
-            subtotal = subtotal - self.big_spender_discount
+
+            subtotal = subtotal - self.bigSpenderDiscount
+
         return subtotal
 
-    def calculate_total(self, is_member, has_coupon):
-        # why i need this? @user
-        subtotal = self.calculate_subtotal()
-        subtotal = self.apply_discounts(subtotal, is_member)
-        total = subtotal + (subtotal * self.tax_rate)
-        if has_coupon == "YES":
-            total = total - (total * self.coupon_discount)
+
+
+    def calculateTotal(self, isMember, hasCoupon):
+
+#why i need this? @user
+
+        subtotal = self.calculateSubtotal()
+
+        subtotal = self.applyDiscounts(subtotal, isMember, hasCoupon)
+
+        total = subtotal + (subtotal * self.taxRate)
+
+        if hasCoupon == "YES":
+
+            total = total - (total * self.couponDiscount)
+
         return total
+
 
 
 def main():
 
-    cart = shoppin_cart()
+    cart = shoppinCart()
+
     item1 = itemz("Apple", 1.5, 10)
+
     item2 = itemz("Banana", 0.5, 5)
+
     item3 = itemz("Laptop", "1000", 1)
+
     item3.category = "electronics"
-    cart.add_item(item1)
-    cart.add_item(item2)
-    cart.add_item(item3)
-    is_member = True
-    has_coupon = "YES"
-    total = cart.calculate_total(is_member, has_coupon)
+
+    cart.addItem(item1)
+
+    cart.addItem(item2)
+
+    cart.addItem(item3)
+
+    isMember = True
+
+    hasCoupon = "YES"
+
+
+
+    total = cart.calculateTotal(isMember, hasCoupon)
+
+
+
     if total < 0:
+
         print("Error in calculation!")
+
     else:
+
         print("The total price is: $" + str(int(total)))
+
 
 
 if __name__ == "__main__":
